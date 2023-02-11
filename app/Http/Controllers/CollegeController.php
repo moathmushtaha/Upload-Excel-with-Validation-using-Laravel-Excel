af60287c0ed98f0e0cbbfb8b2df129f01e7f61a8
moaz mushtaha <moazmoshtha@gmail.com>
2023-02-11 16:29:48 +0200

    finish applications per last 5 year

--- app/Http/Controllers/CollegeController.php
+++ app/Http/Controllers/CollegeController.php
@@ -10,6 +10,7 @@ class CollegeController extends Controller
     {
         $lastFiveYears = collect(range(date('Y') - 4, date('Y')));
 
+<<<<<<< HEAD
         $colleges = College::with('programs')->get();
 
         $colleges = $colleges->each(function ($college) {
@@ -39,5 +40,37 @@ class CollegeController extends Controller
         });
 
         return response()->json($colleges);
+=======
+        $application_status = 1;
+        $college_id = 1;
+        $program_id = 1;
+
+        $college = College::where('college_id', $college_id)->with('programs')->first();
+        if ($college === null) {
+            return response()->json(['message' => 'College not found'], 404);
+        }
+
+        $program = $college->programs->where('program_id', $program_id)->first();
+        if ($program === null) {
+            return response()->json(['message' => 'Program not found'], 404);
+        }
+
+        $applicationsPerYear = $program->applications()
+            ->where('status', $application_status)
+            ->where('acad_year', '>=', date('Y') - 4)
+            ->selectRaw('acad_year, count(*) as count')
+            ->groupBy('acad_year')
+            ->get();
+
+        $college = $lastFiveYears->map(function ($year) use ($applicationsPerYear) {
+            $application = $applicationsPerYear?->firstWhere('acad_year', $year);
+            return [
+                'year' => $year,
+                'count' => $application ? $application->count : 0,
+            ];
+        });
+
+        return response()->json($college);
+>>>>>>> 91354f8 (finish applications per last 5 year)
     }
 }
