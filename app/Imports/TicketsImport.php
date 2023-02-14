44148b3442a412ad628461978918342ecc388938
moaz mushtaha <moazmoshtha@gmail.com>
2023-02-14 17:31:58 +0200

    check if port_protocol and nvt_name not null

--- app/Imports/TicketsImport.php
+++ app/Imports/TicketsImport.php
@@ -3,6 +3,7 @@
 namespace App\Imports;
 
 use App\Models\Ticket;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\SkipsOnFailure;
 use Maatwebsite\Excel\Concerns\ToModel;
@@ -13,8 +14,8 @@ use Maatwebsite\Excel\Validators\Failure;
 class TicketsImport implements ToModel, WithHeadingRow, WithValidation, SkipsOnFailure
 {
     public static array $excel_ips;
-    protected string $port_protocol;
-    protected string $nvt_name;
+    protected mixed $port_protocol;
+    protected mixed $nvt_name;
 
     /**
      * @param array $row
@@ -46,11 +47,18 @@ class TicketsImport implements ToModel, WithHeadingRow, WithValidation, SkipsOnF
     {
         return [
             'ip' => function ($attribute, $value, $onFailure) {
-                $is_exists = DB::table('tickets')
-                    ->where('ip_address', $value)
-                    ->where('port_protocol', $this->port_protocol)
-                    ->where('nvt_name', $this->nvt_name)
-                    ->whereIn('status', [1, 2, 3])->first();
+                $is_exists = DB::table('tickets');
+                $is_exists = $is_exists->where('ip_address', $value);
+
+                if ($this->port_protocol) {
+                    $is_exists = $is_exists->where('port_protocol', $this->port_protocol);
+                }
+
+                if ($this->nvt_name) {
+                    $is_exists = $is_exists->where('nvt_name', $this->nvt_name);
+                }
+
+                $is_exists = $is_exists->whereIn('status', [1, 2, 3])->exists();
 
                 self::$excel_ips [] = $value;
 
@@ -63,7 +71,7 @@ class TicketsImport implements ToModel, WithHeadingRow, WithValidation, SkipsOnF
 
     public function prepareForValidation($data)
     {
-        $data['timestamp'] = \Carbon\Carbon::parse($data['timestamp'])->toDateTimeString();
+        $data['timestamp'] = Carbon::parse($data['timestamp'])->toDateTimeString();
 
         $this->port_protocol = $data['port_protocol'];
         $this->nvt_name = $data['nvt_name'];
